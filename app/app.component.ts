1cff3dbbc23d5329f0f96bb4360812d03e3b86ca
NemanjaT1992 <nemanjatomasevic71@gmail.com>
2016-08-16 22:49:17 +0200

    configured

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { ValidationComponent } from './validation/validation.component';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: "app/app.component.html",
+  directives: [ ValidationComponent ]
 })
 export class AppComponent { }
---
+++ app/validation/validation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Panel } from 'primeng/primeng';
+
+@Component({
+    selector: "validationForm",
+    templateUrl: "app/validation/validation.component.html",
+    directives: [ Panel ]
+})
+export class ValidationComponent
+{
+
+}
